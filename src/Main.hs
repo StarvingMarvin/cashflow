af70034d563275666248ad2d430a6504dca2da59
Luka Stojanovic <luka@magrathea.rs>
2013-03-27 13:52:24 +0100

    getting command arguments

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,14 @@
 
 module Main where
 
+import System.Environment( getArgs )
 import Cashflow.Data.Entry
 
 -- |Cashflow application
 main :: IO()
 main = do
-    putStrLn "CashFlow"
+    args <- getArgs
+    case length args of
+        1 -> putStrLn $ head args
+        _ -> putStrLn "Expected input file as argument"
 
