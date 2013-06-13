120e07874481a99d7f09c7c60e66e51dadc8f894
Luka Stojanovic <luka@magrathea.rs>
2013-06-13 18:47:40 +0100

    basic types

--- Cashflow/Data/Entry.hs
+++ Cashflow/Data/Entry.hs
@@ -2,9 +2,47 @@
 module Cashflow.Data.Entry 
 where
 
-data Entry = OneTimeExpence Int
-    | RecurringExpence Int
-    | Installments Int Int
-    | Income Int
-    | Assets Int
+data Month = Jan | Feb | Mar | Apr | May | Jun 
+            | Jul | Aug | Sep | Oct | Nov | Dec
+    deriving (Show, Read, Eq, Ord, Bounded, Enum)
+
+data Entry = Entry {description :: String, ammount :: Int} 
+    deriving (Show)
+
+class SpecificEntry a where
+    entry   :: a -> Entry
+
+
+data Expence = Expence {
+        expenceEntry    :: Entry,
+        month           :: Month,
+        tentative       :: Bool
+    } deriving (Show)
+
+data MonthlyExpence = MonthlyExpence Entry deriving (Show)
+
+data Income = Income Entry deriving (Show)
+
+data Debt = Debt {
+        debtEntry   :: Entry,
+        creditor    :: String,
+        start       :: Month,
+        instalments :: Int
+    } deriving (Show)
+
+instance SpecificEntry Expence where
+    entry = expenceEntry
+
+instance SpecificEntry MonthlyExpence where
+    entry (MonthlyExpence e) = e
+
+instance SpecificEntry Income where
+    entry (Income e) = e
+
+instance SpecificEntry Debt where
+    entry = debtEntry
+
+sum :: (SpecificEntry a) => [a] -> Int
+sum = foldl (\acc -> (acc +) . ammount . entry) 0
+
 
