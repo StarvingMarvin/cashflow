422b926e38185c2c1c173240a1ca970e91d04d78
Luka Stojanovic <luka@magrathea.rs>
2013-06-15 12:34:33 +0100

    parser

--- Cashflow/Data/Entry.hs
+++ Cashflow/Data/Entry.hs
@@ -6,7 +6,10 @@ data Month = Jan | Feb | Mar | Apr | May | Jun
             | Jul | Aug | Sep | Oct | Nov | Dec
     deriving (Show, Read, Eq, Ord, Bounded, Enum)
 
-data Entry = Entry {description :: String, ammount :: Int} 
+intToMonth :: Int -> Month
+intToMonth = toEnum . pred
+
+data Entry = Entry {entryDescription :: String, entryAmmount :: Int} 
     deriving (Show)
 
 class SpecificEntry a where
@@ -14,9 +17,9 @@ class SpecificEntry a where
 
 
 data Expence = Expence {
-        expenceEntry    :: Entry,
-        month           :: Month,
-        tentative       :: Bool
+        expenceEntry        :: Entry,
+        expenceMonth        :: Month,
+        expenceTentative    :: Bool
     } deriving (Show)
 
 data MonthlyExpence = MonthlyExpence Entry deriving (Show)
@@ -24,11 +27,18 @@ data MonthlyExpence = MonthlyExpence Entry deriving (Show)
 data Income = Income Entry deriving (Show)
 
 data Debt = Debt {
-        debtEntry   :: Entry,
-        creditor    :: String,
-        start       :: Month,
-        instalments :: Int
+        debtEntry       :: Entry,
+        debtCreditor    :: String,
+        debtStart       :: Month,
+        debtInstalments :: Int
     } deriving (Show)
+
+data Entries = Entries {
+        expenceEntries        :: [Expence],
+        monthlyExpenceEntries :: [MonthlyExpence],
+        incomeEntries         :: [Income],
+        debtEntries           :: [Debt]
+    }
 
 instance SpecificEntry Expence where
     entry = expenceEntry
@@ -42,7 +52,24 @@ instance SpecificEntry Income where
 instance SpecificEntry Debt where
     entry = debtEntry
 
-sum :: (SpecificEntry a) => [a] -> Int
-sum = foldl (\acc -> (acc +) . ammount . entry) 0
+entrySum :: (SpecificEntry a) => [a] -> Int
+entrySum = foldl (\acc -> (acc +) . entryAmmount . entry) 0
 
+outstandingDebt :: Debt -> Month -> Int
+outstandingDebt d m = div (months * ammount) instalments
+    where ammount       = entryAmmount $ debtEntry d
+          instalments   = debtInstalments d
+          start         = fromEnum $ debtStart d
+          months        = start + instalments - fromEnum m
+
+project :: Entries -> Month -> Month -> Int
+project e start end = net
+    where
+        monthly     = entrySum $ monthlyExpenceEntries e
+        exp         = entrySum $ expenceEntries e
+        inc         = entrySum $ incomeEntries e
+        months      = [start..end]
+        monthCount  = length months
+        debt        = sum $ map (\d -> outstandingDebt d start) $ debtEntries e
+        net         = (inc - monthly) * monthCount - debt
 
--- cashflow.hs
+++ cashflow.hs
@@ -1,6 +1,11 @@
 
-import Cashflow.Data.Entry
+import qualified Data.Time.Clock as T
+import qualified Data.Time.Calendar as Cal
+import Cashflow.Data
+import Cashflow.Parser
 
 main = do
-    putStrLn "CashFlow"
+    utc <- T.getCurrentTime
+    let day = T.utctDay utc
+    putStrLn $ "CashFlow " ++ Cal.showGregorian day
 
